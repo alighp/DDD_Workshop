1724a9a48b2cc9e9ab02fa6faabf7501be961e4c
a.ghanaatpishe <alighanaatpisheh@gmail.com>
2023-11-28 23:00:16 +0330

    test(money): add money operators test

--- DomainTests/MoneySpecs.cs
+++ DomainTests/MoneySpecs.cs
@@ -1,11 +1,14 @@
 using AutoFixture;
 using AutoFixture.Xunit2;
 using FluentAssertions;
+using Newtonsoft.Json.Linq;
+
 namespace DomainTests;
 
 
 public class MoneySpecs
 {
+
     static T A<T>(Func<T, T>? customization = null)
     {
         var t = new Fixture().Create<T>();
@@ -41,21 +44,105 @@ public class MoneySpecs
     [Theory, AutoData]
     public void Money_cannot_be_negative(decimal amount)
     => new Action(() =>               //Arrange
+
        new Money(-Math.Abs(amount))   //Act
-       ).Should().Throw<Exception>(); //Assert
+    
+    ).Should().Throw<Exception>(); //Assert
 
     [Theory, AutoData]
-    public void Supports_subtraction(uint five)
+    public void Supports_subtraction(decimal left, decimal right)
     {
         //Arrange
-        var smallerNumber = aValidMoney();
-        var biggerNumber = new Money(smallerNumber.Value + five);
+        Money first = Math.Abs(left);
+        Money second = Math.Abs(right);
 
         //Act
-        (biggerNumber - smallerNumber)
+        (left >= right ? first - second : second - first)
 
         //Assert
-        .Value.Should().Be(five);
+        .Value.Should().Be(Math.Abs(left-right));
     }
 
+    [Theory, AutoData]
+    public void Supports_addition(decimal left, decimal right)
+    {
+        //Arrange
+        Money first = Math.Abs(left);
+        Money second = Math.Abs(right);
+
+        //Act
+        (first + second)
+
+        //Assert
+        .Value.Should().Be(left + right);
+    }
+
+    [Theory, AutoData]
+    public void Supports_greater_than(decimal value)
+    {
+        //Arange
+        Money smallerNumber = Math.Abs(value);
+        Money biggerNumber = Math.Abs(value + 2);
+
+        //Act
+        (biggerNumber > smallerNumber)
+
+        //Assert
+        .Should().BeTrue();
+    }
+
+    [Theory, AutoData]
+    public void Supports_lower_than(decimal value)
+    {
+        //Arange
+        Money smallerNumber = Math.Abs(value);
+        Money biggerNumber = Math.Abs(value + 2); ;
+
+        //Act
+        (smallerNumber < biggerNumber)
+
+        //Assert
+        .Should().BeTrue();
+    }
+
+    [Theory, AutoData]
+    public void Supports_equal_or_greater_than(decimal value)
+    {
+        //Arange
+        Money smallerNumber = Math.Abs(0);
+        Money biggerNumber = Math.Abs(0 * 2);
+
+        //Act
+        (biggerNumber >= smallerNumber)
+
+        //Assert
+        .Should().BeTrue();
+    }
+
+    [Theory, AutoData]
+    public void Supports_equal_or_lower_than(decimal value)
+    {
+        //Arange
+        Money smallerNumber = Math.Abs(value);
+        Money biggerNumber = Math.Abs(value * 2);
+
+        //Act
+        (smallerNumber <= biggerNumber)
+
+        //Assert
+        .Should().BeTrue();
+    }
+
+    [Theory, AutoData]
+    public void Supports_assignment_with_decimal_value(decimal oldValue, decimal newValue)
+    {
+        //Arange
+        var money = new Money(oldValue);
+
+        //Act
+        money = newValue;
+
+        //Assert
+        money.Value.Should().Be(newValue);
+    }
 }
